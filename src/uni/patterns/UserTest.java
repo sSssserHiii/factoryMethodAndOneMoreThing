6ddb8bce780ac131b835f1b2b22673e0bac48714
sSssserHiii <s.kruzhanovskiy@stud.onu.edu.ua>
2022-11-16 18:39:38 +0200

    User test2

---
+++ src/uni/patterns/UserTest.java
@@ -0,0 +1,20 @@
+package uni.patterns;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getEnTranslate()
+    {
+        assertAll(this::getEnTranslate);
+    }
+
+    @Test
+    void getRuTranslate()
+    {
+     assertAll(this::getRuTranslate);
+    }
+}
